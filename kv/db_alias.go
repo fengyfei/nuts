e04677a7d6f3a7b54a75d9c4eaddab2ff0fd901f
yangchenglong11 <1332456142@qq.com>
2017-09-17 21:31:59 +0800

    添加 k/v 数据库抽象 #4

---
+++ kv/db_alias.go
@@ -0,0 +1,79 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Co., Ltd..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/09/16        Yang Chenglong
+ */
+
+package kv
+
+import (
+	"sync"
+
+	"github.com/fengyfei/nuts/kv/general"
+)
+
+type DriverType int
+
+const (
+	DRBbolt DriverType = 0
+)
+
+type Alias struct {
+	general.Store
+}
+
+var (
+	drivers = map[string]DriverType{
+		"bbolt": DRBbolt,
+	}
+
+	dbManager = sync.Map{}
+)
+
+func Register(driver string, init general.InitFun) {
+	if init == nil {
+		panic("store: Register init is nil")
+	}
+
+	d, ok := drivers[driver]
+	if !ok {
+		panic("store: Unsupported driver" + driver)
+	}
+
+	if _, ok := dbManager.Load(d); ok {
+		panic("store: Register called twice for init " + driver)
+	}
+
+	dbManager.Store(d, init)
+}
+
+func (a *Alias) Init(name string) {
+	for _, i := range drivers {
+		if f, ok := dbManager.Load(i); ok {
+			a.Store = f.(general.InitFun)(name)
+		}
+	}
+}
---
+++ kv/store/bbolt/db_bbolt.go
@@ -0,0 +1,122 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 SmartestEE Co., Ltd..
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+ * Revision History:
+ *     Initial: 2017/09/16        Yang Chenglong
+ */
+
+package store
+
+import (
+	"errors"
+	"fmt"
+	"encoding/json"
+
+	"github.com/coreos/bbolt"
+	
+	"github.com/fengyfei/nuts/kv"
+	"github.com/fengyfei/nuts/kv/general"
+)
+
+type dbBaseBbolt struct {
+	db *bolt.DB
+}
+
+func init() {
+	kv.Register("bbolt", NewBbolt)
+}
+
+func NewBbolt(name string) general.Store {
+	db := new(dbBaseBbolt)
+	db.init(name)
+	return db
+}
+
+func (bb *dbBaseBbolt) init(path string) error {
+	db, err := bolt.Open(path, 0666, nil)
+	if err != nil {
+		fmt.Printf("[open] init db error: %v", err)
+		return err
+	}
+	bb.db = db
+
+	tx, err := bb.db.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	return tx.Commit()
+}
+
+func (bb *dbBaseBbolt) Put(bucket string, key interface{}, value interface{}) error {
+	return bb.db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			return err
+		}
+		if err := b.Put(convertToByte(key), convertToByte(value)); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func (bb *dbBaseBbolt) Get(bucket string, key interface{}) ([]byte, error) {
+	tx, err := bb.db.Begin(false)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	b := tx.Bucket([]byte(bucket))
+	v := b.Get(convertToByte(key))
+	if v == nil {
+		err := errors.New("get no record")
+		return nil, err
+	}
+
+	return v, nil
+}
+
+func (bb *dbBaseBbolt) Delete(bucket string, key interface{}) error {
+	return bb.db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			return err
+		}
+		if err := b.Delete(convertToByte(key)); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func convertToByte(v interface{}) []byte {
+	b, _ := json.Marshal(v)
+	return b
+}
